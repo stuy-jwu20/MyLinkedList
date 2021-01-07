10c4618c38609bbf8b39ce7b2b756d32ec05d88f
Jonathan Wu <jwu20@stuy.edu>
2021-01-06 21:52:58 -0500

    added mylinkedlist skeleton methods

---
+++ MyLinkedList.java
@@ -0,0 +1,45 @@
+public class MyLinkedList {
+  private int size;
+
+  private Node start, end;
+
+  public MyLinkedList() {
+
+  }
+
+  public int size() {
+
+  }
+
+  public boolean add (String value) {
+
+  }
+
+  public boolean add (int, index, String value) {
+
+  }
+
+  public void add (int index, String valud) {
+
+  }
+
+  public String get (int index) {
+
+  }
+
+  public String set (int index, String value) {
+
+  }
+
+  public String toString() {
+
+  }
+
+  public String reversedToString() {
+
+  }
+
+  private String nodeFinder(int index) {
+
+  }
+}
